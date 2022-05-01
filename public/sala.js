b1ab0a65ed34228fdb2509cb8830d3643ee14c16
Eliás Yousef <eliasyousef00@gmail.com>
2022-05-01 09:13:26 -0300

    projeto

---
+++ public/sala.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById("criar_sala")
+const input = document.getElementById("nome_sala")
+const select = document.getElementById("select_room")
+
+btn.addEventListener('click', (event) => {
+    let salaNova = document.createElement("option")
+    salaNova.value = `${input.value}`
+    salaNova.textContent = input.value
+
+    select.appendChild(salaNova)
+})
